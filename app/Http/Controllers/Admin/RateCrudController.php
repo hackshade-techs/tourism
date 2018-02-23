fd686a284ad30b057d8184a544c7e93f9b476467
hackshade-techs <hackshadetechs@gmail.com>
2018-02-23 11:27:59 +0300

    Added Export Buttons on Models

--- app/Http/Controllers/Admin/RateCrudController.php
+++ app/Http/Controllers/Admin/RateCrudController.php
@@ -81,7 +81,7 @@ class RateCrudController extends CrudController
         // ------ DATATABLE EXPORT BUTTONS
         // Show export to PDF, CSV, XLS and Print buttons on the table view.
         // Does not work well with AJAX datatables.
-        // $this->crud->enableExportButtons();
+        $this->crud->enableExportButtons();
 
         // ------ ADVANCED QUERIES
         // $this->crud->addClause('active');
